280b1dbc71028a2c8bae21ad7afa1d5e1091e09a
Rafalrrr159 <rafal.rrr555@gmail.com>
2025-06-13 01:29:56 +0200

    Add Negotiation DTOs

---
+++ PriceNegotiationAPI/DTOs/Negotiation/CreateNegotiationDTOcs.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceNegotiationAPI.DTOs.Negotiation
+{
+    public class CreateNegotiationDto
+    {
+        [Required]
+        public Guid ProductId { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Proposed price must be greater than zero.")]
+        public decimal ProposedPrice { get; set; }
+    }
+}
---
+++ PriceNegotiationAPI/DTOs/Negotiation/NegotiationDTO.cs
@@ -0,0 +1,18 @@
+﻿using PriceNegotiationAPI.DTOs.Product;
+using PriceNegotiationAPI.Enums;
+
+namespace PriceNegotiationAPI.DTOs.Negotiation
+{
+    public class NegotiationDto
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public decimal ProposedPrice { get; set; }
+        public NegotiationStatus Status { get; set; }
+        public DateTime ProposedDate { get; set; }
+        public int AttemptsCount { get; set; }
+        public DateTime? LastRejectionDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
