848f7a654f962f32413c18b1aed6033727ed6de7
Rafalrrr159 <rafal.rrr555@gmail.com>
2025-06-12 23:33:10 +0200

    Implement Products API Controller

--- PriceNegotiationAPI/Controllers/ProductController.cs
+++ PriceNegotiationAPI/Controllers/ProductController.cs
@@ -1,11 +1,66 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PriceNegotiationAPI.DTOs.Product;
+using PriceNegotiationAPI.Exceptions;
+using PriceNegotiationAPI.Services;
 
 namespace PriceNegotiationAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/products")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class ProductsController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct([FromBody] CreateProductDto dto)
+        {
+            try
+            {
+                var productId = await _productService.CreateProductAsync(dto);
+                return CreatedAtAction(nameof(GetProductById), new { id = productId }, productId);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while creating the product.");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetProductById([FromRoute] Guid id)
+        {
+            try
+            {
+                var product = await _productService.GetProductByIdAsync(id);
+                return Ok(product);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while retrieving the product.");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllProducts()
+        {
+            try
+            {
+                var products = await _productService.GetAllProductsAsync();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while retrieving products.");
+            }
+        }
     }
 }
